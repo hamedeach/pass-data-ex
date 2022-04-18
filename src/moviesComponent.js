39b8b878208076b0041a0ca5582c09c8974e6585
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-04-18 12:17:42 +0200

    finish the instrument 3

--- src/moviesComponent.js
+++ src/moviesComponent.js
@@ -1,45 +1,43 @@
-import React, { Component } from "react";
-
-class MoviesComponent extends Component {
-
-    render() {
-        const users = this.props['usersList'];
-        const movie = this.props['movie'];
-        const profiles = this.props['profilesdata'];
-        const likedusers = profiles.filter(profile => profile.favoriteMovieID == movie.id).map((profileobj) => users[profileobj.userID]);
+import React from "react";
 
 
+// use the a statless function instead of class copmonent
+const MoviesComponent =(props)=>{
+    const users = props['usersList'];
+    const movie = props['movie'];
+    const profiles = props['profilesdata'];
+    const likedusers = profiles.filter(profile => profile.favoriteMovieID == movie.id).map((profileobj) => users[profileobj.userID]);
 
-        return (
-            <li key={movie['id']}>
-                <div key={movie['id']}>
-                    <h2>{movie['name']}</h2>
-                    <p>Liked By:</p>
-                    <ul>
-                        {
-                            (likedusers.length === 0) ?
-                                (<li> no one like this shit</li>)
-                                :
-                                (
+    return (
+        <li key={movie['id']}>
+            <div key={movie['id']}>
+                <h2>{movie['name']}</h2>
+                <p>Liked By:</p>
+                <ul>
+                    {
+                        (likedusers.length === 0) ?
+                            (<li> no one like this shit</li>)
+                            :
+                            (
 
-                                    likedusers.map((userobj) => (
-                                        <li key={userobj.id}>
-                                            {userobj.name}
-                                        </li>
-                                    ))
-                                )
-
-
-                        }
-                    </ul>
-                </div>
-
-
-            </li>
+                                likedusers.map((userobj) => (
+                                    <li key={userobj.id}>
+                                        {userobj.name}
+                                    </li>
+                                ))
+                            )
 
 
-        )
-    }
+                    }
+                </ul>
+            </div>
+
+
+        </li>
+    )
 
 }
+
+
+
 export default MoviesComponent;
--- src/myEx.js
+++ src/myEx.js
@@ -1,24 +1,22 @@
-import React,{Component} from "react";
+import React from "react";
 
-
-class myEX extends Component{
-    render(){
-        const users = this.props['usersList'];
-        const movies = this.props['moviesList'];
-        const profiles = this.props['profilesdata'];
-        return (
-            <ol>
-                {profiles.map((profile)=>(
-                    <li key={profile.id}>{
-                        //get user name from the users list by the profile number 
-                        `${users[profile.userID].name}'s favorite movie is ${movies[profile.favoriteMovieID].name}`
-                    }</li>
-                ))}
-            </ol>
-        )
-
-
-    }
+// use the stateless function instead of the class component
+const myEX = (props)=>{
+    const users = props['usersList'];
+    const movies = props['moviesList'];
+    const profiles = props['profilesdata'];
+    return (
+        <ol>
+            {profiles.map((profile)=>(
+                <li key={profile.id}>{
+                    //get user name from the users list by the profile number 
+                    `${users[profile.userID].name}'s favorite movie is ${movies[profile.favoriteMovieID].name}`
+                }</li>
+            ))}
+        </ol>
+    )
 }
+
+
 
 export default myEX;
